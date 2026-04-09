1c963bf7d4aee0eb3570739100e2576270a2b0e4
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2026-04-08 17:09:53 -0700

    compiles

--- programs/program1/trace.c
+++ programs/program1/trace.c
@@ -1,2 +1,99 @@
 #include "trace.h"
 
+
+void ethernet(const unsigned char *packet, int packet_num){
+}
+void arp(const unsigned char *packet){}
+void ip(const unsigned char *packet){}
+void icmp(const unsigned char *packet, int ip_len){}
+void tcp(const unsigned char *packet, int ip_len){}
+void udp(const unsigned char *packet, int ip_len){}
+
+
+int main(int argc, char *argv[]) {
+
+
+    /*
+    Ethernet -> ARP or IP
+    If IP -> read header
+        can be ICMP, TCP, UDP
+        
+    */
+
+
+
+    pcap_t *handle;
+    char errbuf[256];
+    struct pcap_pkthdr *header;
+    const unsigned char *packet;
+    int result;
+    int packet_num = 1;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <trace file>\n", argv[0]);
+        return 1;
+    }
+
+    handle = pcap_open_offline(argv[1], errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "pcap_open_offline failed: %s\n", errbuf);
+        return 1;
+    }
+
+    while ((result = pcap_next_ex(handle, &header, &packet)) >= 0) {
+        ethernet_o *eth;
+        uint16_t ether_type;
+
+        if (result == 0) {
+            continue;
+        }
+
+        if (header->caplen < sizeof(ethernet_o)) {
+            continue;
+        }
+
+        eth = (ethernet_o *) packet;
+        ether_type = ntohs(eth->ether_type);
+
+        printf("Packet number: %d\n", packet_num);
+        ethernet(packet, packet_num);
+
+        if (ether_type == 0x0806) {
+            arp(packet + sizeof(ethernet_o));
+        } else if (ether_type == 0x0800) {
+            ip_o *ip_hdr;
+            int ip_len;
+
+            if (header->caplen < sizeof(ethernet_o) + sizeof(ip_o)) {
+                packet_num++;
+                continue;
+            }
+
+            ip_hdr = (ip_o *)(packet + sizeof(ethernet_o));
+            ip_len = (ip_hdr->ver_ihl & 0x0F) * 4;
+
+            ip(packet + sizeof(ethernet_o));
+
+            if (ip_hdr->protocol == 1) {
+                icmp(packet + sizeof(ethernet_o) + ip_len, ip_len);
+            } else if (ip_hdr->protocol == 6) {
+                tcp(packet + sizeof(ethernet_o) + ip_len, ip_len);
+            } else if (ip_hdr->protocol == 17) {
+                udp(packet + sizeof(ethernet_o) + ip_len, ip_len);
+            } else {
+                printf("what is this");
+            }
+        }
+        printf("\n");
+        packet_num++;
+    }
+
+    if (result == -1) {
+        fprintf(stderr, "error");
+        pcap_close(handle);
+        return 1;
+    }
+
+    pcap_close(handle);
+    return 0;
+}
--- programs/program1/trace.h
+++ programs/program1/trace.h
@@ -1,12 +1,83 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <pcap.h>
+#include <arpa/inet.h>
+
+void ethernet(const unsigned char *packet, int packet_num);
+void arp(const unsigned char *packet);
+void ip(const unsigned char *packet);
+void icmp(const unsigned char *packet, int ip_header_len);
+void tcp(const unsigned char *packet, int ip_header_len);
+void udp(const unsigned char *packet, int ip_header_len);
+
 
 #pragma pack(push, 1)
-typedef struct{
-    uint32_t header;
-    
-}packet;
+typedef struct {
+    uint8_t dst_mac[6];
+    uint8_t src_mac[6];
+    uint16_t ether_type;
+} ethernet_o;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    uint8_t type;
+    uint8_t code;
+    uint16_t checksum;
+
+    uint16_t identifier;
+    uint16_t seq_num;
+} icmp_o;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    uint16_t htype;
+    uint16_t ptype;
+    uint8_t hlen;
+    uint8_t plen;
+    uint16_t oper;
+
+    uint8_t sha[6];  // sender hardware addr
+    uint8_t spa[4];  // sender protocol addr
+    uint8_t tha[6];  // target hardware addr
+    uint8_t tpa[4];  // target protocol addr
+} arp_o;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    uint8_t ver_ihl;        // version + header length
+    uint8_t tos;
+    uint16_t total_length;
+    uint16_t id;
+    uint16_t flags_frag;
+
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t checksum;
+
+    uint32_t src_ip;
+    uint32_t dst_ip;
+} ip_o;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    uint16_t src_port;
+    uint16_t dst_port;
+
+    uint32_t seq_num;
+    uint32_t ack_num;
+
+    uint8_t data_offset;   // upper 4 bits used
+    uint8_t flags;
+
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urgent_ptr;
+} tcp_o;
 #pragma pack(pop)
 
